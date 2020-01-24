b7ed99223060e6ba34c481a5f7150a64e202e4d5
Jun Aruga <junaruga@users.noreply.github.com>
2020-01-24 17:52:54 +0100

    Add CLOCK_REALTIME logic as ok pattern. (#3)

--- src/main.c
+++ src/main.c
@@ -7,15 +7,23 @@ int main(void)
     struct timespec tp;
 
     memset(&tp, 0, sizeof(struct timespec));
-    if (clock_gettime(CLOCK_BOOTTIME, &tp) == -1) {
-        perror("clock_gettime CLOCK_BOOTTIME");
+    if (clock_gettime(CLOCK_REALTIME, &tp) == -1) {
+        perror("clock_gettime CLOCK_REALTIME");
         return 1;
     }
+
+    memset(&tp, 0, sizeof(struct timespec));
+    if (clock_gettime(CLOCK_BOOTTIME, &tp) == -1) {
+        perror("clock_gettime CLOCK_BOOTTIME");
+        return 2;
+    }
+
     memset(&tp, 0, sizeof(struct timespec));
     if (clock_gettime(CLOCK_TAI, &tp) == -1) {
         perror("clock_gettime CLOCK_TAI");
-        return 1;
+        return 3;
     }
+
     printf("Done successfully.\n");
 
     return 0;
